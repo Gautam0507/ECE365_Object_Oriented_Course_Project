4cd1dc60dc14538e5ffff9064c913e35252d20e9
Gautam <gautam.nraj@gmail.com>
2025-03-19 16:28:35 +0530

    copied over the basic testcases given by prof

---
+++ tests/TestCases/test/Test00_FilesDontExistOrWrongFormat/run.bat
@@ -0,0 +1,5 @@
+..\..\x64\Debug\Maze2D.exe Maze2D.datNOTFOUND Path1D.dat
+
+..\..\x64\Debug\Maze2D.exe Maze2D.dat         Path1D.datNOTFOUND
+
+..\..\x64\Debug\Maze2D.exe Path1D.dat Maze2D.dat  & :: Reversed the order of the files
---
+++ tests/TestCases/test/Test10_InvalidPathHavingMoreNonZeroEntriesEvenAfterReachingEndofMaze/run.bat
@@ -0,0 +1,1 @@
+..\..\x64\Debug\Maze2D.exe Maze2D.dat Path1D.dat
